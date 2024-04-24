d57faa965b8c2f4a046175019ef8966e89fac129
mintesnot afework <mintesnotafework12@gmail.com>
2024-04-24 23:29:49 +0300

    create a public private key pair for the server

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
+python ./note/cryptoengine.py --no-input
 
 uwsgi --socket :8000 --master --enable-threads --module secure_note.wsgi
