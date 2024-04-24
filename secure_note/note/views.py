c03c70a1ed2045a5b79655547a9e3997ca67b8ef
mintesnot afework <mintesnotafework12@gmail.com>
2024-04-24 21:53:54 +0300

    making it clear and oop based

--- secure_note/note/views.py
+++ secure_note/note/views.py
@@ -4,30 +4,33 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from .models import FileModel
 from django.contrib import Views
-
-# Create your views here.
-@login_required(login_url="/login/")
-def index(requests):
-    list_of_file = FileModel.objects.filter(user=requests.user)
-    return render(requests,"note/index.html",{"list_of_file":list_of_file})
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
 
-@login_required(login_url="/login/")
-def save_file(requests):
-    if requests.method == 'POST':
+class IndexView(LoginRequiredMixin,View):
+    login_url="/login/"
+    def get(self,requests):
+        list_of_file = FileModel.objects.filter(user=requests.user)
+        return render(requests,"note/index.html",{"list_of_file":list_of_file})
+    
+
+class SaveFile(LoginRequiredMixin,View):
+    login_url="/login/"
+    def get(self,requests):
+        return HttpResponseRedirect(reverse("note:index"))
+    
+    def post(self,requests):
         file_name = requests.POST.get('filename')
         file_content = requests.POST.get('filecontent')
         if (file_name is not None):
             try:
-                temp = FileModel.objects.get(name=file_name)
-                if temp.user == requests.user:
-                    print("mintens jsdlkfjsak")
-                    raise IndexError()
-            except IndexError:
-                m = FileModel.objects.get(name = file_name)
+                temp = FileModel.objects.filter(user=requests.user).get(name=file_name)
+                raise FileExistsError()
+            except FileExistsError:
+                m = temp
                 m.content = file_content
                 m.save()
-                print(m.content)
             except:
                 m = FileModel()
                 m.name = file_name
@@ -35,19 +38,20 @@ def save_file(requests):
                 m.user = requests.user
                 m.save()
             return HttpResponseRedirect(reverse("note:file_display",args = (file_name,)))
-    return HttpResponseRedirect(reverse("note:index"))
 
 
-@login_required(login_url="/login/")
-def file_display(requests, name):
-    file = FileModel.objects.get(name=name)
-    list_of_files = FileModel.objects.filter(user=requests.user)
-    return render(requests,"note/index2.html",{"file":file,"list_of_file":list_of_files})
-
-
-@login_required(login_url="/login/")
-def delete_file(requests,name):
-    if requests.method == "POST":
+class FileDisplayView(LoginRequiredMixin,View):
+    login_url = "/login/"
+    def get(self,requests,name):
         file = FileModel.objects.get(name=name)
-        file.delete()
-    return HttpResponseRedirect(reverse("note:index"))
+        list_of_files = FileModel.objects.filter(user=requests.user)
+        return render(requests,"note/index2.html",{"file":file,"list_of_file":list_of_files})
+
+
+class DeleteFileView(LoginRequiredMixin,View):
+    login_url = "/login/"
+    def get(self,requests,name):
+        if requests.method == "POST":
+            file = FileModel.objects.get(name=name)
+            file.delete()
+        return HttpResponseRedirect(reverse("note:index"))
