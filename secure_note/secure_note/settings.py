ff06167c924968fefaa4ad94c7b58d583994fbf9
mintesnot afework <mintesnotafework12@gmail.com>
2024-04-25 14:58:22 +0300

    adding link to about and forget page and modify the validation adn setting file for deploument

--- secure_note/secure_note/settings.py
+++ secure_note/secure_note/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY","change_me")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://localhost","https://127.0.0.1"]
