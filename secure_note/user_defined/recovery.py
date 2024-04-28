b466770cbb637db486926e954499193efe42b679
mintesnot afework <mintesnotafework12@gmail.com>
2024-04-28 16:35:31 +0300

    clinnet public and private key regenertion

--- secure_note/user_defined/recovery.py
+++ secure_note/user_defined/recovery.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from note.models import FileModel
 from secure_note.user_defined import cryptoengine
 import random
+import os
 
 
 class Recover:
@@ -10,11 +11,23 @@ class Recover:
     def RegenerateAESKey(user: User,userprofile:UserProfile) -> UserProfile:
         random_value = str(random.randbytes(10))
         aes_key = cryptoengine.AESCryptography.key_generation(random_value)
-        userprofile = Recover.ReEncryptFile(user,userprofile,aes_key)
+        if userprofile.previous_hashed_password is not None:
+            Recover.reGenerateRSAKey(user,userprofile)
+        userprofile = Recover.reEncryptFile(user,userprofile,aes_key)
         return userprofile
 
     @staticmethod
-    def ReEncryptFile(user:User,userprofile:UserProfile,aes_key:bytes) -> UserProfile:
+    def reGenerateRSAKey(user:User,userprofile:UserProfile) -> None:
+        file_path = userprofile.file_path
+        aes_key = cryptoengine.RSACryptography.decryption(file_path,userprofile.hashed_password)
+        cryptoengine.RSACryptography.key_generation(file_path)
+        userprofile.previous_hashed_password = userprofile.hashed_password
+        userprofile.hashed_password = cryptoengine.RSACryptography.encryption(file_path,aes_key)
+        userprofile.signed_password = cryptoengine.RSACryptography.sign(userprofile.hashed_password)
+        userprofile.save()
+
+    @staticmethod
+    def reEncryptFile(user:User,userprofile:UserProfile,aes_key:bytes) -> UserProfile:
         file_list = FileModel.objects.filter(user=user)
         aes_old_key = cryptoengine.RSACryptography.decryption(userprofile.file_path, userprofile.hashed_password)
         for i in file_list:
